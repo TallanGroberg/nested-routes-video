774133ed78bb8b6d0e5d5d4b3d105dc2130a24a4
tallan groberg <tallan.taven@gmail.com>
2020-04-28 18:57:14 -0400

    final thing

--- src/Character.js
+++ src/Character.js
@@ -1,14 +1,24 @@
 import React, {useState, useEffect} from 'react';
-
+import Axios from 'axios';
+import {useParams} from 'react-router-dom'
 const Character = () => {
-  
+  const [person, setPerson] = useState({})
+
+const name = useParams().name
+
+  useEffect( () => {
+    Axios.get('https://swapi.py4e.com/api/people/?search=' + name)
+    .then( res => {
+      setPerson(res.data.results[0])
+    })
+  }, [])
 
 
     
   return (
     <div>
       Character
-      
+      {person.name}
     </div>
   );
 };
